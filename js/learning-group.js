78a169e12f3aef107e87e18c43c43a563c4560e0
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2022-04-13 17:13:12 +0700

    Update learning-group.js

--- js/learning-group.js
+++ js/learning-group.js
@@ -32,7 +32,7 @@ function Connect_DB() {
   //LoadVDOGroup();
   //ShowVDOList();
 }
- 
+
 
 function getParameterByName(name, url) {
   str = '';
@@ -133,7 +133,7 @@ function ShowVDOList() {
       str += '<div class="col-lg-6 col-md-2 slide text-center boxvdo" data-aos="fade-left" onclick="OpenVdo(\''+ doc.id +'\','+i+')">';
       str += '<div class="boxvdo-border member"><div class="boxvdo-img">';
       str += '<img src="'+doc.data().VDOimg+'" class="img-fluid" style="border-radius: 10px;">';
-      str += '<div class="vdo-timer"><img src="./img/timer.png" width="15px"> '+doc.data().VDOtimer+'</div></div>'; 
+      str += '<div class="vdo-timer"><img src="./img/timer.png" width="15px"> '+doc.data().VDOtimer+'</div></div>';
       str += '<div class="boxvdo-line1">'+doc.data().VDOdetail+'</div>';
       str += '<div class="boxvdo-line2"><div class="boxvdo-icon1">';
       str += '<img src="./img/calendar.png" class="boxvdoimg"> <span>'+doc.data().VDOdate+'</span></div>';
